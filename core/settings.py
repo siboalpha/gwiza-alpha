efeea73d589de48c8840027fbef93398668de986
siboalpha <sibo.alphonsee@gmail.com>
2022-11-10 09:22:16 +0200

    debug of secret key error

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 import os
 
 #load_dotenv()
-load_dotenv("/home/admin/gwizaalpha/core/.env")
+load_dotenv("/home/admin/gwiza-alpha/core/.env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
